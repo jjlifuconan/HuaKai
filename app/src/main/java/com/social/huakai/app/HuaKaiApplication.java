854e8ea6de5a0cc51f1d302126c28b493a6e5d75
fulijing <conanaiflj@gmail.com>
2019-07-01 15:36:59 +0800

    im uikit

--- app/src/main/java/com/social/huakai/app/HuaKaiApplication.java
+++ app/src/main/java/com/social/huakai/app/HuaKaiApplication.java
@@ -3,7 +3,13 @@ package com.social.huakai.app;
 import android.support.multidex.MultiDexApplication;
 
 import com.example.http.HttpUtils;
+import com.netease.nim.uikit.api.NimUIKit;
+import com.netease.nimlib.sdk.NIMClient;
+import com.netease.nimlib.sdk.auth.LoginInfo;
+import com.netease.nimlib.sdk.util.NIMUtil;
 import com.social.basecommon.util.DebugUtil;
+import com.social.huakai.im.NimSDKOptionConfig;
+
 
 /**
  * @author Administrator
@@ -22,5 +28,36 @@ public class HuaKaiApplication extends MultiDexApplication {
         super.onCreate();
         application = this;
         HttpUtils.getInstance().init(this, DebugUtil.DEBUG);
+        // 初始化云信SDK
+        NIMClient.init(this, getLoginInfo(), NimSDKOptionConfig.getSDKOptions(this));
+
+        // 以下逻辑只在主进程初始化时执行
+        if (NIMUtil.isMainProcess(this)) {
+            // 在主进程中初始化UI组件，判断所属进程方法请参见demo源码。
+            initUiKit();
+        }
     }
+
+    private LoginInfo getLoginInfo() {
+//        String account = Preferences.getUserAccount();
+////        String token = Preferences.getUserToken();
+////
+////        if (!TextUtils.isEmpty(account) && !TextUtils.isEmpty(token)) {
+////            DemoCache.setAccount(account.toLowerCase());
+////            return new LoginInfo(account, token);
+////        } else {
+            return null;
+//        }
+    }
+
+    private void initUiKit() {
+        // 初始化
+        NimUIKit.init(this);
+
+    }
+
+
+
+
+
 }
